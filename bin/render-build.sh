2f3ad84e6093a046b9174ee83fc51db95767a38f
luisaugusto <luis.augusto.u@gmail.com>
2024-08-21 19:45:20 -0500

    cambie el render-build segun cursor

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
+bundle exec rails assets:clobber
+RAILS_ENV=production bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
@@ -11,3 +12,10 @@ bundle exec rails assets:clean
 # Uncomment the following line:
 
 bundle exec rails db:migrate
+
+# Print out some debugging information
+echo "Listing precompiled assets:"
+ls -la public/assets
+
+echo "Checking for match_controller.js:"
+find public/assets -name "*match_controller*"
